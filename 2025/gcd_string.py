1d7ccac5c16054072eb178466d50f19d37aecd63
navyabattula <navyabio12@gmail.com>
2025-10-07 18:59:55 +0530

    Adding code

---
+++ 2025/gcd_string.py
@@ -0,0 +1,31 @@
+"""
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+"""
+import math
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        len1 = len(str1)
+        len2 = len(str2)
+
+        gcd = math.gcd(len1, len2)
+
+        return str1[0:gcd]
