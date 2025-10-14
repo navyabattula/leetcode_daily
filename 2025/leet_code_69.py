a2d45d5f7ce8d17f16ec30a4922f7aacf43694b5
navyabattula <navyabio12@gmail.com>
2025-10-14 19:29:54 +0530

    Adding code

---
+++ 2025/leet_code_69.py
@@ -0,0 +1,25 @@
+"""
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        left, right = 1, x
+
+        while left<= right:
+
+            mid = (left+right)//2
+
+            if mid*mid == x:
+                return mid
+            elif mid*mid > x:
+                right = mid - 1
+            elif mid*mid < x:
+                left = mid +1
+
+        return right
